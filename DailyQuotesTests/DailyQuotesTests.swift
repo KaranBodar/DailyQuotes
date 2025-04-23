d4e5975dd2db102ce5e54d66ca81956b38b2d8c6
KaranBodar <karanbodar.adtech@gmail.com>
2025-04-23 16:49:08 +0530

    App UI created

---
+++ DailyQuotesTests/DailyQuotesTests.swift
@@ -0,0 +1,17 @@
+//
+//  DailyQuotesTests.swift
+//  DailyQuotesTests
+//
+//  Created by Karan Bodar on 23/04/25.
+//
+
+import Testing
+@testable import DailyQuotes
+
+struct DailyQuotesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
